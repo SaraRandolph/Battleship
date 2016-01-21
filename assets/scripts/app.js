575357629e1a9f47ebc84f08a2a53f40b5262d9e
Sara Randolph <sara.randolph222@gmail.com>
2016-01-20 21:48:25 -0700

    day 2 added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,7 @@ grid.length = 3;
 var ship = {
   isHit:false
 }
-// kajdsflkjlfkajasfdssdfa
+
 
 var missilesFired = 0; 
 var rows = ["A","B","C","D","E","F","G","H", "I","J"]
@@ -63,6 +63,53 @@ function createGrid (rows){
     } for (var col = 0; col <rows.length; i++){
         grid[row][col] = {name: row + col}
     }
+}
+
+function fireMissle(){
+    var coords = document.getElementById('user-guess').value;
+    coords = parseInput(coords);
+    var row = coords[0];
+    var col = coords[1];
+    checkHit(coords.row,coords.col);
+}
+
+function parseInput(coords){
+    var char1 = coords[0].toUpperCase();
+    var char2 = parseInt(coords[1]) - 1
+    var usersShot =  {row:char1 , col:char2};
+    return usersShot;
+}
+
+function checkHit(row, col){
+    var cell = grid[row][col]
+    if (cell.firedAt){
+        alert('Invalid Location: Already fired at ' + row + col)
+        return
+    } else {
+        cell.firedAt = true;
+    } if (cell.ship){
+        cell.ship.hits++;
+        cell.text = 'Hit!'
+        if (cell.ship.isSunk()){
+            alert("You sank my battleship. Must be luck!");
+        }else {
+            cell.firedAt = true;
+        }
+    }
+}
+
+function checkVictory(){
+    var shipsSunk = 0
+    for (var i = 0; i < ships.length; i++){
+        if (ships[i].isSunk()){
+            shipsSunk++;
+        }
+    }
+        if (shipsSunk >= ships.length){
+            return true;
+        } else {
+            return false;
+        }
 }
 
 hideShip(); 
